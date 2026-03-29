7452de49cf0d5dd230f377dd4155b302457d0fbb
A <a.zaborin@g.nsu.ru>
2026-03-29 15:57:40 +0500

    Realize empty controller

---
+++ src/test/java/com/ChanelManager/FulfillmentService/FulfillmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ChanelManager.FulfillmentService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FulfillmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
